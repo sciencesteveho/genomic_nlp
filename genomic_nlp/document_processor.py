57a52ccc37a318192190b77c5514d7e0903a3fd2
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2024-09-20 21:49:41 -0400

    Fix: adjust batch size

--- genomic_nlp/document_processor.py
+++ genomic_nlp/document_processor.py
@@ -179,7 +179,7 @@ class ChunkedDocumentProcessor:
         abstracts: pd.DataFrame,
         chunk: int,
         genes: Set[str],
-        batch_size: int = 16,
+        batch_size: int = 96,
     ):
         """Initialize the ChunkedDocumentProcessor object."""
         self.root_dir = root_dir
