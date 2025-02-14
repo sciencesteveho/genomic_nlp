e2650358d59846dc723141e3b71bb1a4d8842f48
sciencesteveho <46689240+sciencesteveho@users.noreply.github.com>
2025-02-14 15:31:06 -0500

    Extend n2v to gda

--- genomic_nlp/train_node2vec.py
+++ genomic_nlp/train_node2vec.py
@@ -7,34 +7,24 @@ relationships."""
 
 
 import argparse
-import gc
 from pathlib import Path
 import pickle
 from typing import Any
 
-from gensim.models import Word2Vec  # type: ignore
-from gensim.models.callbacks import CallbackAny2Vec  # type: ignore
 from grape import Graph  # type: ignore
 import grape  # type: ignore
 from grape.embedders import Node2VecSkipGramEnsmallen  # type: ignore
-import networkx as nx  # type: ignore
-from node2vec import Node2Vec  # type: ignore
 import numpy as np
-import pandas as pd
 
 
-class EpochSaver(CallbackAny2Vec):
-    """Callback to save model after every epoch."""
-
-    def __init__(self, savedir: str):
-        self.epoch = 0
-        self.savedir = savedir
-
-    def on_epoch_end(self, model: Any) -> None:
-        """Save model after every epoch."""
-        print(f"Save model number {self.epoch}.")
-        model.save(f"{self.savedir}/model_epoch{self.epoch}.pkl")
-        self.epoch += 1
+def save_embeddings(embeddings: Any, index: int, model_dir: Path, outname: Any) -> None:
+    """Convert ensmallen embeddings to dict and save to file."""
+    embedding_vectors = embeddings._node_embeddings[index].to_dict("index")
+    embedding_vectors = {
+        k: np.array(list(v.values())) for k, v in embedding_vectors.items()
+    }
+    with open(model_dir / outname, "wb") as f:
+        pickle.dump(embedding_vectors, f)
 
 
 def main() -> None:
@@ -52,18 +42,24 @@ def main() -> None:
         help="Directory to save the model.",
         default="/ocean/projects/bio210019p/stevesho/genomic_nlp/models/n2v",
     )
+    parser.add_argument(
+        "--embedding_type",
+        type=str,
+        help="Embedding type.",
+        default="disease",
+        choices=["ppi", "disease"],
+    )
     args = parser.parse_args()
-    edge_file = f"/ocean/projects/bio210019p/stevesho/genomic_nlp/ppi/gene_co_occurence_{args.year}.tsv"
+    if args.embedding_type == "ppi":
+        edge_file = f"/ocean/projects/bio210019p/stevesho/genomic_nlp/ppi/gene_co_occurence_{args.year}.tsv"
+    elif args.embedding_type == "disease":
+        edge_file = f"/ocean/projects/bio210019p/stevesho/genomic_nlp/training_data/disease/gda_co_occurence_{args.year}.tsv"
+    else:
+        raise ValueError("Invalid embedding type. Must be 'ppi' or 'disease'.")
 
     # make year dir
-    model_dir = Path(args.model_dir) / args.year
+    model_dir = Path(f"{args.model_dir}/{args.embedding_type}") / args.year
     model_dir.mkdir(parents=True, exist_ok=True)
-
-    # # load edges
-    # edges = pd.read_csv(
-    #     edge_file, sep="\t", header=None, names=["source", "destination"]
-    # )
-    # edges = edges.drop_duplicates()
 
     graph = grape.Graph.from_csv(
         edge_path=edge_file,
@@ -73,49 +69,43 @@ def main() -> None:
     )
     print("Graph loaded.")
 
-    # create graph
-    # graph = nx.from_pandas_edgelist(edges, create_using=nx.Graph)
-
-    # instantiate model
-    # node2vec = Node2Vec(
-    #     graph,
-    #     dimensions=128,
-    #     walk_length=40,
-    #     num_walks=10,
-    #     workers=24,
-    # )
     embeddings = Node2VecSkipGramEnsmallen(embedding_size=128).fit_transform(graph)
 
-    # train node2vec
-    # model = node2vec.fit(
-    #     window=10,
-    #     min_count=1,
-    #     batch_words=4,
-    #     callbacks=[EpochSaver(str(model_dir))],
-    # )
-
-    # try and clear memory before saving
-    # gc.collect()
-
-    # model.save(str(model_dir / "node2vec.model"))
-    # save embeddings
-
-    # convert ensmallen embeddings to dict
-    embedding_vectors = embeddings._node_embeddings[0].to_dict("index")
-    embedding_vectors = {
-        k: np.array(list(v.values())) for k, v in embedding_vectors.items()
-    }
-    with open(model_dir / "input_embeddings.pkl", "wb") as f:
-        pickle.dump(embedding_vectors, f)
-
-    # save output embeddings
-    output_embeddings = embeddings._node_embeddings[1].to_dict("index")
-    output_embeddings = {
-        k: np.array(list(v.values())) for k, v in output_embeddings.items()
-    }
-    with open(model_dir / "output_embeddings.pkl", "wb") as f:
-        pickle.dump(output_embeddings, f)
+    save_embeddings(embeddings, 0, model_dir, "input_embeddings.pkl")
+    save_embeddings(embeddings, 1, model_dir, "output_embeddings.pkl")
 
 
 if __name__ == "__main__":
     main()
+
+# train node2vec
+# model = node2vec.fit(
+#     window=10,
+#     min_count=1,
+#     batch_words=4,
+#     callbacks=[EpochSaver(str(model_dir))],
+# )
+
+# try and clear memory before saving
+# gc.collect()
+
+# model.save(str(model_dir / "node2vec.model"))
+# save embeddings
+
+# create graph
+# graph = nx.from_pandas_edgelist(edges, create_using=nx.Graph)
+
+# instantiate model
+# node2vec = Node2Vec(
+#     graph,
+#     dimensions=128,
+#     walk_length=40,
+#     num_walks=10,
+#     workers=24,
+# )
+
+# # load edges
+# edges = pd.read_csv(
+#     edge_file, sep="\t", header=None, names=["source", "destination"]
+# )
+# edges = edges.drop_duplicates()
